8f6b1aed5124e19b8d682b32bd74b544bacc564c
sofyanegi <sofyanegil@gmail.com>
2023-04-19 14:51:10 +0700

    implement in main

--- main.go
+++ main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"time"
 
 	"mygram-api/config"
+	"mygram-api/controllers"
+	"mygram-api/helpers"
 	"mygram-api/models"
+	"mygram-api/repository"
+	"mygram-api/routers"
+	"mygram-api/services"
 )
 
 func main() {
@@ -19,4 +26,42 @@ func main() {
 	db.Table("photos").AutoMigrate(&models.Photo{})
 	db.Table("comments").AutoMigrate(&models.Comment{})
 	db.Table("social_media").AutoMigrate(&models.SocialMedia{})
+
+	//Init Repository
+	userRepository := repository.NewUsersRepositoryImpl(db)
+	photoRepository := repository.NewPhotoRepositoryImpl(db)
+	commentRepository := repository.NewCommentRepositoryImpl(db)
+	socialMediaRepository := repository.NewSocialMediaRepositoryImpl(db)
+
+	//Init Service
+	authenticationService := services.NewAuthenticationServiceImpl(userRepository)
+
+	//Init controller
+	authenticationController := controllers.NewAuthenticationController(authenticationService)
+	userController := controllers.NewUserController(userRepository)
+	photoController := controllers.NewPhotoController(photoRepository)
+	commentController := controllers.NewCommentController(commentRepository)
+	socialMediaController := controllers.NewSocialMediaController(socialMediaRepository)
+
+	routes := routers.NewRouter(
+		userRepository,
+		userController,
+		photoRepository,
+		photoController,
+		commentRepository,
+		commentController,
+		socialMediaRepository,
+		socialMediaController,
+		authenticationController,
+	)
+	server := &http.Server{
+		Addr:           ":" + loadConfig.ServerPort,
+		Handler:        routes,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	server_err := server.ListenAndServe()
+	helpers.ErrorPanic(server_err)
 }
